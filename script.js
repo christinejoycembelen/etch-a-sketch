07d671183513e83da398c310b1c27c256fdc3e17
Christine Joyce Belen <christinejoycembelen@gmail.com>
2026-09-21 10:45:50 +0800

    Add grid and mouseover function to mimic etch-a-sketch behavior

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ const GRID_SIZE = 600;
 let squaresPerSide = 16;
 
 const sketchArea = document.querySelector("#sketch-area");
+const sliderContainer = document.querySelector("#slider-container");
+const slider = document.querySelector("#slider");
+const sliderValue = document.querySelector("#slider-value");
+
+sliderValue.textContent = `${slider.value} x ${slider.value} (Resolution)`; 
 sketchArea.style.width = sketchArea.style.height = `${GRID_SIZE}px`;
 
-function createCells() {
+function createCells(squaresPerSide) {
     const numOfSquares = (squaresPerSide * squaresPerSide);
     const widthOrHeight = `${(GRID_SIZE / squaresPerSide) - 2}px`;
 
@@ -24,4 +29,17 @@ function changeColor() {
     this.style.backgroundColor = "black";
 }
 
-createCells();
+function removeGridCells() {
+    while (sketchArea.firstChild) {
+        sketchArea.removeChild(sketchArea.firstChild);
+    }
+}
+
+slider.oninput = function () {
+    let txt = `${this.value} x ${this.value} (Resolution)`;
+    sliderValue.innerHTML = txt;
+    removeGridCells();
+    createCells(this.value);
+}
+
+createCells(16);
